3da56d1fee3380b912027ce128c5a06d716df61a
Tomasz Wlodarczyk <tomwlo@gmail.com>
2023-11-28 07:36:14 -0800

    include negative weights and super tf

--- tests/testthat/test-regulonSimilarities.R
+++ tests/testthat/test-regulonSimilarities.R
@@ -1,12 +1,14 @@
-regulon <- data.frame(tf = rep(LETTERS[1:4], times = c(5,5,6,3)), target = NA)
-regulon$target[regulon$tf=="A"] <- LETTERS[5:9]
+regulon <- data.frame(tf = rep(LETTERS[1:5], times = c(5,5,6,3,1)), target = NA)
+regulon$target[regulon$tf=="A"] <- LETTERS[6:10]
 regulon$target[regulon$tf=="B"][c(1,3,5)] <- regulon$target[regulon$tf=="A"][c(1,2,4)]
 regulon$target[regulon$tf=="C"][6] <- regulon$target[regulon$tf=="A"][3]
-regulon$target[is.na(regulon$target)] <- sample(LETTERS[10:14], sum(is.na(regulon$target)), replace = TRUE)
+regulon$target[regulon$tf=="E"] <- "B"
+regulon$target[is.na(regulon$target)] <- sample(LETTERS[11:15], sum(is.na(regulon$target)), replace = TRUE)
 regulon$weights <- runif(nrow(regulon))
+regulon$weights <- regulon$weights * sample(c(1,-1), length(regulon$weights),replace = TRUE)
 
-res <- vector(mode = "list", 3)
-names(res) <- LETTERS[2:4]
+res <- vector(mode = "list", 4)
+names(res) <- LETTERS[2:5]
 res <- lapply(res, function(x) data.frame(target=character(0), focal_weight=numeric(0),
                                           other_tf_weight=numeric(0), weight_product=numeric(0)))
 
@@ -26,5 +28,5 @@ test_graph <- buildGraph(regulon)
 partners_graph <- findPartners(test_graph, "A")
 
 test_that("findPartners function works correctly", {
-  expect_identical(partners_graph, res)
+  expect_identical(partners_graph[order(names(partners_graph))], res[order(names(res))])
 })
