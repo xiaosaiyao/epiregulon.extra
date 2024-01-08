5fbcd7d5de9a69222e4e611765cd6b807b1b8824
Tomasz Wlodarczyk <tomwlo@gmail.com>
2024-01-08 05:17:37 -0800

    vignette added

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' regulon created using `epiregulon` package from reprogam-seq data
+#' @name regulon
+#' @docType data
+#' @usage data(regulon)
+#' @format a DFrame.
+#' @return a DFrame.
+#' @keywords datasets
+#' @examples
+#' data(regulon)
+"regulon"
